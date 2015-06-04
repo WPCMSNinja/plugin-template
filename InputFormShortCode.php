f476cb8c23f1a582b27d22dd8757640a6c5b268c
Michael Simpson <michael.d.simpson@gmail.com>
2015-06-04 14:51:33 -0400

    changed download button and added fork on git button

--- InputFormShortCode.php
+++ InputFormShortCode.php
@@ -31,6 +31,53 @@ class InputFormShortCode extends PluginTemplate_ShortCodeScriptLoader {
 //                    var pt = new PluginTemplateValidator();
 //                });
             </script>
+
+        <style type="text/css">
+            .button {
+                display: inline-block;
+                text-align: center;
+                vertical-align: middle;
+                padding: 12px 24px;
+                border: 1px solid #a12727;
+                border-radius: 8px;
+                background: #ff4a4a;
+                background: -webkit-gradient(linear, left top, left bottom, from(#ff4a4a), to(#992727));
+                background: -moz-linear-gradient(top, #ff4a4a, #992727);
+                background: linear-gradient(to bottom, #ff4a4a, #992727);
+                text-shadow: #591717 1px 1px 1px;
+                font: normal normal bold 20px arial;
+                color: #ffffff;
+                text-decoration: none;
+            }
+            .button:hover,
+            .button:focus {
+                background: #ff5959;
+                background: -webkit-gradient(linear, left top, left bottom, from(#ff5959), to(#b62f2f));
+                background: -moz-linear-gradient(top, #ff5959, #b62f2f);
+                background: linear-gradient(to bottom, #ff5959, #b62f2f);
+                color: #ffffff;
+                text-decoration: none;
+            }
+            .button:active {
+                background: #982727;
+                background: -webkit-gradient(linear, left top, left bottom, from(#982727), to(#982727));
+                background: -moz-linear-gradient(top, #982727, #982727);
+                background: linear-gradient(to bottom, #982727, #982727);
+            }
+            .button:before{
+                content:  "\0000a0";
+                display: inline-block;
+                height: 24px;
+                width: 24px;
+                line-height: 24px;
+                margin: 0 4px -6px -4px;
+                position: relative;
+                top: 0px;
+                left: 0px;
+                background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABrUlEQVRIibWWv0oDQRDGfzmDZSpJISLhvCr4BBJEySukDD5GSBXIq0QsAvZyRrER8QW0MgbEKl3qcGixs9zc3t3eKfrBwmTnz+582Zm5Bn5EwADoA8fAnuyvgVfgHrgG3iri5BACc2ALfFWsrdiGdYMPgU2NwO7aiK8XYyBRTglwI44hsCsrlL24wH7ku7k2fgFOlP4MmAITkS16YqsPyWUSkqXlAWg5NlOlnzq6lvhoujL/ydy5uRscubm1mRToW04mc6uISF9LQpaWuhlY9Ehp3gJRgHnnTTG4BZ5KnOvgEbgTuQkMAkwRWVx5nJMS2cWlkvsBpkItfLffKZFdPCu5G5CWP8Cnx7EuPpTcDn7g6KOlFE1M49qX3wfAu8gXwJEKfK78TjEvKcHQtQRmojtUdmuABenz0xUYAivKe49dK7JFNVS6BZj+YzdiJ8OO3K4s+FJsNGKlH0N1oXVKMlkVBM8VmlVUtQqXLpcW8LQKG6Cq2XUwlBTRUtnsoLhd9woOcYPXatcWI/IDJBaHiHTgRPxi4OhM/m1kWvzZ0G9UHKQ/W7pAW/Zrf7Z8A9NN2vCvMa7nAAAAAElFTkSuQmCC") no-repeat left center transparent;
+                background-size: 100% 100%;
+            }
+        </style>
     <form id="PluginTemplateForm" action="<?php echo $this->actionUrl ?>" method="post">
         <table>
             <tbody>
@@ -86,7 +133,6 @@ class InputFormShortCode extends PluginTemplate_ShortCodeScriptLoader {
         <tr>
             <td>
                 <div id="DonateButton">
-                    Suggested donation: $10
                     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_paypal">
                         <input type="hidden" name="cmd"
                                value="_s-xclick">
@@ -101,7 +147,10 @@ class InputFormShortCode extends PluginTemplate_ShortCodeScriptLoader {
                 </div>
             </td>
             <td>
-                <button onclick="validatePluginForm()">Download</button>
+                <a class="button" onclick="validatePluginForm()">Download</a>
+            </td>
+            <td>
+                <iframe src="https://ghbtns.com/github-btn.html?user=mdsimpson&repo=plugin-template&type=fork&count=false&size=large" frameborder="0" scrolling="0" width="158px" height="30px"></iframe>
             </td>
         </tr>
         </tbody>
